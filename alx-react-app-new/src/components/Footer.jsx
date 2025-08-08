57e88a9dcc45b649df5f62570285230033eeca5a
yabsira Hanne <yabsirahanne@gmail.com>
2025-08-09 01:36:04 +0300

    react-update

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer style={{ backgroundColor: '#eee', textAlign: 'center', padding: '10px', marginTop: '20px' }}>
+      <p>&copy; 2025 City Explorer. All rights reserved.</p>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MainContent = () => {
+  return (
+    <main style={{ padding: '20px', backgroundColor: '#f0f8ff', color: '#333' }}>
+      <p>Welcome to the city explorer app. Discover amazing places and stories!</p>
+    </main>
+  );
+};
+
+export default MainContent;
