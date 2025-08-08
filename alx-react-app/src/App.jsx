4a16db1302db4754a655dae70ad63abde6fb2ea3
yabsira Hanne <yabsirahanne@gmail.com>
2025-08-08 13:56:31 +0300

    edit

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -10,6 +10,7 @@ function App() {
   return (
     <>
       <div>
+        <WelcomeMessage />
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
