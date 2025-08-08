316f9704db0344389bd3e5c95a19f5ad64543d10
yabsira Hanne <yabsirahanne@gmail.com>
2025-08-08 14:10:34 +0300

    =

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,9 +1,11 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+importimport reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import WelcomeMessage from './components/WelcomeMessage'
-
+import Header from  './Header'
+import MainContent from './MainContent'
+import Footer from './Footer'
 function App() {
   const [count, setCount] = useState(0)
 
@@ -11,6 +13,9 @@ function App() {
     <>
       <div>
         <WelcomeMessage />
+        <Footer />
+        <Header />
+        <MainContent />
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
