80664f09c0af213810803e88692fb7715690d18a
yeab <yeab@yeab.yeab>
2025-08-11 04:38:01 -0400

    update

--- tailwind-react-integration/vite.config.js
+++ tailwind-react-integration/vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.ts
 
-// https://vite.dev/config/
+import { defineConfig } from 'vite'
+    import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite' // ✅ Add this
+
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+        react(),
+        tailwindcss(), // ✅ Add this
+  ],
 })
