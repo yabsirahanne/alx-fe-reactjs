6c5de8c2cf59411a95d3f8e289bcb7a5dc738293
yeab <yeab@yeab.yeab>
2025-08-12 19:17:25 -0400

    my first company

---
+++ my-company/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <footer style={{ padding: '10px', backgroundColor: '#eee', textAlign: 'center' }}>
+      <p>&copy; {new Date().getFullYear()} My Company. All rights reserved.</p>
+    </footer>
+  );
+}
+
+export default Footer;
