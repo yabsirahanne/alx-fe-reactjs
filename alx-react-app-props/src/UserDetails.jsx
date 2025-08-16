f4356514fd8d6077170dd8859d5365c48728c26c
yeab <yeab@yeab.yeab>
2025-08-16 04:00:49 -0400

    ......

--- alx-react-app-props/src/UserDetails.jsx
+++ alx-react-app-props/src/UserDetails.jsx
@@ -1,4 +1,9 @@
-function UserDetails({ userData }) {
+import { useContext } from 'react';
+import UserContext from './UserContext';
+
+function UserDetails() {
+  const userData = useContext(UserContext);
+
   return (
     <div>
       <p>Name: {userData.name}</p>
