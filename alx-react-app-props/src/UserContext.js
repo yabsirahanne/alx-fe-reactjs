14035854fb1e56c777ebbb8a7f7fa7e0ec2ecdf5
yeab <yeab@yeab.yeab>
2025-08-16 06:13:38 -0400

    ......

--- alx-react-app-props/src/UserContext.js
+++ alx-react-app-props/src/UserContext.js
@@ -1,4 +1,6 @@
-import { createContext } from 'react';
+// alx-react-app-props/src/UserContext.js
+// This file creates a UserContext using React's createContext API
+// import { createContext } from 'react';
 
 const UserContext = createContext();
 
