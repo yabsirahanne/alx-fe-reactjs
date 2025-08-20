358bb5ba276c232ba964c603e2ef8e4cadb8fe38
yeab <yeab@yeab.yeab>
2025-08-20 09:17:44 -0400

    ....

---
+++ recipe-sharing-app/src/Components/RecipeList.jsx
@@ -0,0 +1,24 @@
+
+import { useRecipeStore } from './recipeStore';
+
+const RecipeList = () => {
+  const recipes = useRecipeStore((state) => state.recipes);
+
+  return (
+    <div>
+      <h2>Recipes</h2>
+      {recipes.length === 0 ? (
+        <p>No recipes yet.</p>
+      ) : (
+        recipes.map((recipe) => (
+          <div key={recipe.id}>
+            <h3>{recipe.title}</h3>
+            <p>{recipe.description}</p>
+          </div>
+        ))
+      )}
+    </div>
+  );
+};
+
+export default RecipeList;
---
+++ recipe-sharing-app/src/Components/recipeStore.js
@@ -0,0 +1,9 @@
+
+import { create } from 'zustand';
+
+export const useRecipeStore = create((set) => ({
+  recipes: [],
+  addRecipe: (newRecipe) =>
+    set((state) => ({ recipes: [...state.recipes, newRecipe] })),
+  setRecipes: (recipes) => set({ recipes }),
+}));
