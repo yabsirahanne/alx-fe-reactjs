e8e17de30d653d8155ec825df5f3e38de81ab4cb
Yabsira hanne <yabsira@gmail.com>
2025-08-30 13:48:03 +0300

    WIP: local changes before merging main

--- recipe-sharing-app/src/store/recipeStore.js
+++ recipe-sharing-app/src/store/recipeStore.js
@@ -1,8 +1,23 @@
+// recipeStore.js
 import { create } from 'zustand';
 
 export const useRecipeStore = create((set) => ({
   recipes: [],
+
   addRecipe: (newRecipe) =>
-    set((state) => ({ recipes: [...state.recipes, newRecipe] })),
-  setRecipes: (recipes) => set({ recipes }),
+    set((state) => ({
+      recipes: [...state.recipes, newRecipe],
+    })),
+
+  updateRecipe: (updatedRecipe) =>
+    set((state) => ({
+      recipes: state.recipes.map((recipe) =>
+        recipe.id === updatedRecipe.id ? updatedRecipe : recipe
+      ),
+    })),
+
+  deleteRecipe: (id) =>
+    set((state) => ({
+      recipes: state.recipes.filter((recipe) => recipe.id !== id),
+    })),
 }));
