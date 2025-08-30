88afbef73c50843ad26c8c551788953551f7e523
Yabsira hanne <yabsira@gmail.com>
2025-08-30 14:38:20 +0300

    recipe store

--- recipe-sharing-app/src/Components/recipeStore.js
+++ recipe-sharing-app/src/Components/recipeStore.js
@@ -1,9 +1,9 @@
-
 import { create } from 'zustand';
 
 export const useRecipeStore = create((set) => ({
   recipes: [],
-  addRecipe: (newRecipe) =>
-    set((state) => ({ recipes: [...state.recipes, newRecipe] })),
-  setRecipes: (recipes) => set({ recipes }),
+  addRecipe: (recipe) => set((state) => ({
+    recipes: [...state.recipes, recipe]
+  })),
+  // other actions...
 }));
